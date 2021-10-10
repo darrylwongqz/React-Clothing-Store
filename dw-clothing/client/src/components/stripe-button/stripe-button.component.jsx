50c7a50a28570d4aeb92072fba2b3f0eee6d60cf
darrylwongqz <dwqz92@gmail.com>
2021-10-10 22:44:40 +0800

    Stripe and Client side separation
    "separated our cra app into client folder, integrated stripe on backend server"

--- dw-clothing/client/src/components/stripe-button/stripe-button.component.jsx
+++ dw-clothing/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_7vs0XkxLHNrjUeUfrcikXZPi00bedvGZT5";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios
+      .post({
+        url: "payment",
+        data: {
+          amount: priceForStripe,
+          token,
+        },
+      })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card"
+        );
+      });
   };
 
   return (
